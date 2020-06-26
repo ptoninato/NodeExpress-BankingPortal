dc6ec1b9ac07330ed30d08a997aec822b9e770d9
ptoninato <pmtoninato@gmail.com>
2020-06-25 21:35:17 -0500

    module 1 complete

--- src/app.js
+++ src/app.js
@@ -4,5 +4,16 @@ const express = require('express');
 
 var app = express();
 app.set('views', path.join(__dirname, './views'));
+app.use('/css', express.static(path.join(__dirname, '/public/css')));
+app.use('/js', express.static(path.join(__dirname, '/public/js')));
 app.set('view engine', 'ejs')
 
+app.get('/', (req, res) => {
+  res.render('index', { 
+    title: 'Index'
+  });
+});
+
+app.listen(3000, () => {
+  console.log("PS Project Running on port 3000!");
+});
