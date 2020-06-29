dfa4ab7691f502d11f9bfef2a4755372c67333ab
ptoninato <pmtoninato@gmail.com>
2020-06-28 21:25:05 -0500

    module 2

--- src/app.js
+++ src/app.js
@@ -8,11 +8,47 @@ app.use('/css', express.static(path.join(__dirname, '/public/css')));
 app.use('/js', express.static(path.join(__dirname, '/public/js')));
 app.set('view engine', 'ejs')
 
+const accountData = fs.readFileSync('./src/json/accounts.json', {
+  encoding: 'UTF8'
+});
+const userData = fs.readFileSync('./src/json/users.json', {
+  encoding: 'UTF8'
+})
+
+const accounts = JSON.parse(accountData);
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
   res.render('index', { 
-    title: 'Index'
+    title: 'Account Summary',
+    accounts: accounts
   });
 });
+
+app.get('/savings', (req, res) => {
+  res.render('account', {
+    account: accounts.savings
+  });
+});
+
+app.get('/checking', (req, res) => {
+  res.render('account', {
+    account: accounts.checking
+  });
+});
+
+app.get('/credit', (req, res) => {
+  res.render('account', {
+    account: accounts.credit
+  });
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile', {
+    user: users[0]
+  });
+});
+
 
 app.listen(3000, () => {
   console.log("PS Project Running on port 3000!");
